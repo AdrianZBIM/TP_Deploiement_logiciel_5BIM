a67b7fcfc6a5da6187ec40cf9a164edb5621f082
Zurmely Adrian <azurmely@bs406-s34-13.insa-lyon.fr>
2021-11-30 11:27:44 +0100

    Part 2 : Docstring auto sphinx

---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. Doc_TP_Deploiement_log_5BIM documentation master file, created by
+   sphinx-quickstart on Tue Nov 30 10:55:40 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Doc_TP_Deploiement_log_5BIM's documentation!
+=======================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   modules
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- main.py
+++ main.py
@@ -3,27 +3,28 @@ import src.complexity_lists as cmp_l
 
 
 
+if __name__ == '__main__':
+	
+	#-----------------------------------------------
+	# MAIN
+	#-----------------------------------------------
+	k=500
 
-#-----------------------------------------------
-# MAIN
-#-----------------------------------------------
-k=500
+	print("temps moyen append : "+str(cmp_l.append_time([1,2,"toto"], k))+"\n")
+	print("temps moyen insert : "+str(cmp_l.inserthead_time([1,2,"toto"], k))+"\n")
+	print("temps moyen access : "+str(cmp_l.access_time([1,2,"toto"], k))+"\n")
 
-print("temps moyen append : "+str(cmp_l.append_time([1,2,"toto"], k))+"\n")
-print("temps moyen insert : "+str(cmp_l.inserthead_time([1,2,"toto"], k))+"\n")
-print("temps moyen access : "+str(cmp_l.access_time([1,2,"toto"], k))+"\n")
+	#print(cmp_l.create_big_list_from_file("src/words.txt", "utf-8"))
+	big_list = cmp_l.create_big_list_from_file("src/words.txt","utf-8")
+	k = 200
+	with open("truc.txt", "w+", 1024, encoding="utf-8") as f:
 
-#print(cmp_l.create_big_list_from_file("src/words.txt", "utf-8"))
-big_list = cmp_l.create_big_list_from_file("src/words.txt","utf-8")
-k = 200
-with open("truc.txt", "w+", 1024, encoding="utf-8") as f:
-
-	for i in range (1000,201000,1000):
-		sublist = big_list[0:i]
-		appendtime = cmp_l.append_time(sublist, k)
-		instime = cmp_l.inserthead_time(sublist, k)
-		f.write("{0:16}".format(str(i)) + "{0:28}".format(str(appendtime))+ "{0:28}".format(str(instime))+"\n")
+		for i in range (1000,201000,1000):
+			sublist = big_list[0:i]
+			appendtime = cmp_l.append_time(sublist, k)
+			instime = cmp_l.inserthead_time(sublist, k)
+			f.write("{0:16}".format(str(i)) + "{0:28}".format(str(appendtime))+ "{0:28}".format(str(instime))+"\n")
 
 
-import doctest
-doctest.testmod(cmp_l, verbose=True)
+	import doctest
+	doctest.testmod(cmp_l, verbose=True)
